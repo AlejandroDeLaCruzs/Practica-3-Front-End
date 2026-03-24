d10943e39cc42c721f926a62e0f49806e7ff0c57
AlejandroDeLaCruzs <alexcruzs1105@gmail.com>
2026-03-24 18:17:06 +0100

    first commit

---
+++ src/app/albums/[id]/page.tsx
@@ -0,0 +1,7 @@
+
+
+export const AlbumId = () => {
+    return (
+        <h1>detalles del album</h1>
+    )
+}
---
+++ src/context/MusicContext.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { createContext, useContext, useState } from "react";
+
+type ContextListaType = {
+  idsFavoritos: string[];
+  addFavorite: (item: string) => boolean;
+  deleteFavorite: (item: string) => boolean;
+};
+
+const Listacontext = createContext<ContextListaType | null>(null);
+
+type Params = {
+  children: React.ReactNode;
+};
+
+export const ContextProvider = ({ children }: Params) => {
+  const [idsFavoritos, setidsFavoritos] = useState<string[]>([]);
+
+  const addFavorite = (item: string) => {
+    const alredyInList = idsFavoritos.find((e) => item == e);
+    if (alredyInList) return false;
+    else {
+      setidsFavoritos([...idsFavoritos, item]);
+      return true;
+    }
+  };
+
+  const deleteFavorite = (item: string) => {
+    const notInList = idsFavoritos.find((e) => item == e);
+    if (!notInList) return false;
+    else {
+      setidsFavoritos([...idsFavoritos, item]);
+      return true;
+    }
+  };
+
+  return (
+    <Listacontext.Provider value={{ idsFavoritos, addFavorite, deleteFavorite }}>
+      {children}
+    </Listacontext.Provider>
+  );
+};
+
+
+export const useLista = () => {
+    const context = useContext(Listacontext);
+    if(!context) {
+        throw new Error ("No puedes acceder al contexto");
+    }
+    return context;
+}
